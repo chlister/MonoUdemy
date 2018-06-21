b237c51e1f98d4b94268608208584882f1350154
Marc Rohwedder Kær <mr-jmp@hotmail.com>
2018-06-21 10:08:27 +0200

    Drawing in different panels

---
+++ GraphicsIntro/GraphicsIntro/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace GraphicsIntro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.GrapTest1 = new System.Windows.Forms.Button();
+            this.GrapTest2 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.panel1.Location = new System.Drawing.Point(35, 29);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(259, 181);
+            this.panel1.TabIndex = 0;
+            // 
+            // GrapTest1
+            // 
+            this.GrapTest1.Location = new System.Drawing.Point(57, 217);
+            this.GrapTest1.Name = "GrapTest1";
+            this.GrapTest1.Size = new System.Drawing.Size(92, 41);
+            this.GrapTest1.TabIndex = 1;
+            this.GrapTest1.Text = "Perform Graphics Test 1";
+            this.GrapTest1.UseVisualStyleBackColor = true;
+            this.GrapTest1.Click += new System.EventHandler(this.GrapTest1_Click);
+            // 
+            // GrapTest2
+            // 
+            this.GrapTest2.Location = new System.Drawing.Point(156, 217);
+            this.GrapTest2.Name = "GrapTest2";
+            this.GrapTest2.Size = new System.Drawing.Size(97, 41);
+            this.GrapTest2.TabIndex = 2;
+            this.GrapTest2.Text = "Perform Graphics Test 2";
+            this.GrapTest2.UseVisualStyleBackColor = true;
+            this.GrapTest2.Click += new System.EventHandler(this.GrapTest2_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel2.Location = new System.Drawing.Point(423, 29);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(286, 181);
+            this.panel2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.GrapTest2);
+            this.Controls.Add(this.GrapTest1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button GrapTest1;
+        private System.Windows.Forms.Button GrapTest2;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ GraphicsIntro/GraphicsIntro/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphicsIntro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Graphics g;
+        Graphics g1;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            g = panel1.CreateGraphics();
+            g1 = panel2.CreateGraphics();
+        }
+
+        private void GrapTest1_Click(object sender, EventArgs e)
+        {
+            g.Clear(panel1.BackColor);
+
+            // Pens are a built in C#. They are 1 pixel wide pens
+            g.DrawLine(Pens.Red, 0, 0, panel1.Width, panel1.Height);
+
+            Pen p = new Pen(Color.FromArgb(245,202,65), 10);
+            g.DrawRectangle(p, 100, 50, 50,100);
+        }
+
+        private void GrapTest2_Click(object sender, EventArgs e)
+        {
+            g1.Clear(panel2.BackColor);
+            SolidBrush deep = new SolidBrush(Color.FromArgb(51, 51, 255));
+            SolidBrush trans = new SolidBrush(Color.FromArgb(80,153, 153, 255)); // Alpha can be set at the beginning of Argb() selector
+            Pen p = new Pen(Color.FromArgb(245,202,65), 10);
+            g1.DrawRectangle(p, 100, 50, 50,100);
+
+            g1.FillRectangle(deep, 75,50,50,120);
+            g1.FillRectangle(trans, 75+50,50,50,120);
+        }
+    }
+}
