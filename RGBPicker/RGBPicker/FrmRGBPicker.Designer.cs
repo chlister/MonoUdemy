7fc772c2f1deb0bf6b96d9a10d26faac23b21ed0
Marc Rohwedder Kær <mr-jmp@hotmail.com>
2018-06-27 15:20:54 +0200

    Adding alpha to color picker

--- RGBPicker/RGBPicker/FrmRGBPicker.Designer.cs
+++ RGBPicker/RGBPicker/FrmRGBPicker.Designer.cs
@@ -35,9 +35,16 @@
             this.numGreenUD = new System.Windows.Forms.NumericUpDown();
             this.label3 = new System.Windows.Forms.Label();
             this.numBlueUD = new System.Windows.Forms.NumericUpDown();
+            this.numHue = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numSaturation = new System.Windows.Forms.NumericUpDown();
+            this.buttonCopy = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.numRedUD)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numGreenUD)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numBlueUD)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numHue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSaturation)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -121,11 +128,78 @@
             this.numBlueUD.TabIndex = 7;
             this.numBlueUD.ValueChanged += new System.EventHandler(this.numBlueUD_ValueChanged);
             // 
+            // numHue
+            // 
+            this.numHue.Location = new System.Drawing.Point(281, 33);
+            this.numHue.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numHue.Name = "numHue";
+            this.numHue.Size = new System.Drawing.Size(62, 20);
+            this.numHue.TabIndex = 8;
+            this.numHue.Value = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numHue.ValueChanged += new System.EventHandler(this.numHue_ValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(281, 14);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Alpha";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(278, 65);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Saturation";
+            // 
+            // numSaturation
+            // 
+            this.numSaturation.Location = new System.Drawing.Point(281, 84);
+            this.numSaturation.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numSaturation.Name = "numSaturation";
+            this.numSaturation.Size = new System.Drawing.Size(62, 20);
+            this.numSaturation.TabIndex = 11;
+            // 
+            // buttonCopy
+            // 
+            this.buttonCopy.Location = new System.Drawing.Point(284, 144);
+            this.buttonCopy.Name = "buttonCopy";
+            this.buttonCopy.Size = new System.Drawing.Size(59, 23);
+            this.buttonCopy.TabIndex = 12;
+            this.buttonCopy.Text = "Copy";
+            this.buttonCopy.UseVisualStyleBackColor = true;
+            this.buttonCopy.Click += new System.EventHandler(this.buttonCopy_Click);
+            // 
             // RGB
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(290, 200);
+            this.ClientSize = new System.Drawing.Size(355, 200);
+            this.Controls.Add(this.buttonCopy);
+            this.Controls.Add(this.numSaturation);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numHue);
             this.Controls.Add(this.numBlueUD);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.numGreenUD);
@@ -139,6 +213,8 @@
             ((System.ComponentModel.ISupportInitialize)(this.numRedUD)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numGreenUD)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numBlueUD)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numHue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numSaturation)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -153,6 +229,11 @@
         private System.Windows.Forms.NumericUpDown numGreenUD;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.NumericUpDown numBlueUD;
+        private System.Windows.Forms.NumericUpDown numHue;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numSaturation;
+        private System.Windows.Forms.Button buttonCopy;
     }
 }
 
--- RGBPicker/RGBPicker/FrmRGBPicker.cs
+++ RGBPicker/RGBPicker/FrmRGBPicker.cs
@@ -13,6 +13,10 @@ namespace RGBPicker
     public partial class RGB : Form
     {
         Graphics g;
+        int redVal;
+        int greenVal;
+        int blueVal;
+        int alphaVal;
         public RGB()
         {
             InitializeComponent();
@@ -22,23 +26,50 @@ namespace RGBPicker
         {
             g = panel1.CreateGraphics();
             g.Clear(panel1.BackColor);
-            panel1.BackColor = Color.FromArgb((int)numRedUD.Value,(int)numGreenUD.Value,(int)numBlueUD.Value);
+            redVal = (int)numRedUD.Value;
+            greenVal = (int)numGreenUD.Value;
+            blueVal = (int)numBlueUD.Value;
+            alphaVal = (int)numHue.Value;
+            panel1.BackColor = Color.FromArgb(alphaVal, redVal, greenVal, blueVal);
+            numHue.Value = (int)panel1.BackColor.A;
+            numSaturation.Value = (int)Math.Ceiling(panel1.BackColor.GetSaturation());
         }
         private void numRedUD_ValueChanged(object sender, EventArgs e)
         {
-            panel1.BackColor = Color.FromArgb((int)numRedUD.Value,(int)numGreenUD.Value,(int)numBlueUD.Value);
+            //panel1.BackColor = Color.FromArgb((int)numRedUD.Value, (int)numGreenUD.Value, (int)numBlueUD.Value);
+            redVal = (int)numRedUD.Value;
+            SetColor();
+
         }
 
         private void numGreenUD_ValueChanged(object sender, EventArgs e)
         {
-            panel1.BackColor = Color.FromArgb((int)numRedUD.Value,(int)numGreenUD.Value,(int)numBlueUD.Value);
+            //panel1.BackColor = Color.FromArgb((int)numRedUD.Value, (int)numGreenUD.Value, (int)numBlueUD.Value);
+            greenVal = (int)numGreenUD.Value;
+            SetColor();
+
         }
 
         private void numBlueUD_ValueChanged(object sender, EventArgs e)
         {
-            panel1.BackColor = Color.FromArgb((int)numRedUD.Value,(int)numGreenUD.Value,(int)numBlueUD.Value);
+            //panel1.BackColor = Color.FromArgb((int)numRedUD.Value, (int)numGreenUD.Value, (int)numBlueUD.Value);
+            blueVal = (int)numBlueUD.Value;
+            SetColor();
+
         }
 
-        
+        private void numHue_ValueChanged(object sender, EventArgs e)
+        {
+            alphaVal = (int)numHue.Value;
+            SetColor();
+        }
+        private void SetColor()
+        {
+            panel1.BackColor = Color.FromArgb(alphaVal, redVal, greenVal, blueVal);
+        }
+
+        private void buttonCopy_Click(object sender, EventArgs e)
+        { 
+        }
     }
 }
